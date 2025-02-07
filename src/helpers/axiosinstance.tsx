e1d18bbf122673ddc54de92bf0f4d9221f7e0d50
rohan <rohanthakur89768@gmail.com>
2025-02-07 19:32:50 +0530

    fixing bug

--- src/helpers/axiosinstance.tsx
+++ src/helpers/axiosinstance.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-//const baseURL: string = "http://localhost:8000";
-const baseURL: string =  "https://expen-track-backend.vercel.app";
+const baseURL: string = "http://localhost:8000";
+//const baseURL: string =  "https://expen-track-backend.vercel.app/";
 const axiosInstance = axios.create({
   baseURL,
 });
